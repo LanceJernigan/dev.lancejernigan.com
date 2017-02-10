0dc04891a15194b225c492ec2984921878fb978b
LanceJernigan <lance@knoxweb.com>
2017-02-10 11:49:11 -0500

    Restyled About Section

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -9,7 +9,7 @@ const About = () => {
 
     return (
 
-        <div>
+        <div >
 
             <Hero />
 
@@ -24,7 +24,14 @@ const About = () => {
                 <Wrapper>
 
                     <p>I currently work as a Javascript Engineer at Knoxweb in Knoxville, TN.  I have experience working in both the LAMP stack, using technologies like PHP and MySQL, and the more recently adopted MERN stack using technologies like Express, Node, React, and Redux.  My work generally consists of using frontend technologies like React and Redux while leveraging the simplicity of Wordpress' content management system.</p>
-                    <p>If any of my experience interests you or you just want to chat, feel free to send me an email at <a href="mailto:lance@lancejernigan.com" target="_blank">Lance@LanceJernigan.com</a>.</p>
+
+                    <div className={styles.divider}></div>
+
+                    <div className={styles.social}>
+
+                        <a target="_blank" href="https://github.com/LanceJernigan"><h5>GitHub</h5></a>
+
+                    </div>
 
                 </Wrapper>
 
